71d815a12939335d2ed4df56e526725de1adc0a6
ClementLiuKaYu <76940127+ClementLiuKaYu@users.noreply.github.com>
2021-03-01 15:41:14 +0800

    Created files
    Created handlebars, css and basic page layouts.
    Created index.js and added basic express setup.
    Created main.js for later use in front end.

---
+++ index.js
@@ -0,0 +1,20 @@
+// Express app setup
+const express = require('express')
+const app = express()
+const port = 8080
+const server = app.listen(port,()=>{console.log(`Finding Memo at port ${port}...`)})
+
+// Importing libraries needed
+const handlebars = require('express-handlebars')
+
+// Handlebars setup
+app.engine('handlebars',handlebars({defaultLayout:'main'}))
+app.set('view engine','handlebars')
+
+// Serves public files
+app.use(express.static('public'))
+
+// Initial get req
+app.get('/',(req,res)=>{
+    res.render('index',{notes:[1,2,3,4,5]})
+})
